a6396a787e894ee9cdffd52229f4dd0eb93ea247
Subrato Sarkar <subratosarkar@Subratos-MacBook-Pro.local>
2021-08-05 14:13:04 -0400

    leetcode problems added

---
+++ leetcode/string/isIsomorphic.js
@@ -0,0 +1,38 @@
+/**
+ * 205. Isomorphic Strings
+Easy
+
+2545
+
+537
+
+Add to List
+
+Share
+Given two strings s and t, determine if they are isomorphic.
+
+Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+
+All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+
+Example 1:
+Input: s = "egg", t = "add"
+Output: true
+
+Example 2:
+Input: s = "foo", t = "bar"
+Output: false
+
+Example 3:
+Input: s = "paper", t = "title"
+Output: true
+ */
+
+var isIsomorphic = function(s, t) {
+    for (let i = 0; i < s.length; i++) {
+        if (s.indexOf(s.charAt(i)) != t.indexOf(t.charAt(i))) {
+            return false;
+        }
+    }
+    return true;
+};
