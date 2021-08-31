afdcbabfb4134e5f2874eb3d59423d197bfd7a6d
Subrato Sarkar <subratosarkar@Subratos-MacBook-Pro.local>
2021-08-31 14:38:19 -0400

    array sorting concept added

--- js-concept-telusko/arrow_function.js
+++ js-concept-telusko/arrow_function.js
@@ -6,3 +6,9 @@ console.log(sum(2, 3));
 // one liner, you write and omit return statement, if this is a one liner code
 let add = (a, b) => a + b;
 console.log(add(2, 3));
+
+//Soring array
+let arr = [5, 4, 7, 2, 3];
+arr.sort((a, b) => a - b);
+
+console.log(arr)
