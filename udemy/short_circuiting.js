2b574b8c3223f2e666a2906241f6bb366a7e4081
Subrato Sarkar <subratosarkar@Subratos-MacBook-Pro.local>
2021-09-04 14:47:49 -0400

    updated short circuting concept

--- udemy/short_circuiting.js
+++ udemy/short_circuiting.js
@@ -8,7 +8,8 @@ console.log(undefined || null); //null
 console.log(undefined || 0 || 'Hello' || 23 || null); //'Hello', because undefined, 0 are falsy value and 'hello' is the first truethy value.
 
 //Short-circuiting(&& operator): works exactly opposite to || operator, i.e. first falsy value will be returned.
-console.log(3 && 'Subrato'); //Subrato
+console.log(0 && 'Subrato'); //0, as 0 is first falsy value;
+console.log(3 && 'Subrato'); //Subrato, as the 1st value is truethy, hence it will move on to next one. No falsy value found, hence it return last value;
 console.log('' && 'Subrato'); //'' because '' is the falsy value
 console.log(true && 0); //0
 console.log(undefined && null); //undefined
