4ebcd8654683248af94df24170457633d00d649f
Subrato Sarkar <subratosarkar@Subratos-MacBook-Pro.local>
2021-10-06 13:16:21 -0400

    event loop concept added

---
+++ udemy/SECTION_16-Asynchronous.js/event_loop_promise.js
@@ -0,0 +1,20 @@
+console.log('start'); //output- 1
+setTimeout(() => console.log('0 sec timer'), 0); //output- 4
+Promise.resolve('Resolved promise 1').then(res => console.log(res)); //output- 3(Precedence over 
+//line 2 because of resolved promise in micro task queue, and line 2 will be call back queue)
+console.log('end'); //output- 2
+
+Promise.resolve('Resolved promise 2').then(res => {
+    for (let i = 0; i < 1000000000; i++) {}
+    console.log(res);
+})
+
+/** o/p
+ *  script.js:1 start
+    script.js:5 end
+    script.js:3 Resolved promise 1
+    script.js:9 Resolved promise 2
+    script.js:2 0 sec timer
+ */
+
+//Micro task queue has a higher precedence to execute over call back queue.
