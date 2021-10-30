a73dd098490c5a151bf42f77c285e1fe33760044
Subrato Sarkar <subratosarkar@Subratos-MacBook-Pro.local>
2021-10-30 13:57:03 -0400

    algoexpert dfs problem added

--- algoexpert/graph/dfs.js
+++ algoexpert/graph/dfs.js
@@ -11,10 +11,10 @@ class Node {
     }
   
     depthFirstSearch(array) {
-      array.push(this.name);
-          for (let child of this.children) {
-              child.depthFirstSearch(array);
-          }
-          return array;
+        array.push(this.name);
+        for (let child of this.children) {
+            child.depthFirstSearch(array);
+        }
+        return array;
     }
 }
