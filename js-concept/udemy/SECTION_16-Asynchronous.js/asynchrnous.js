910096747301eae5e9853bd22c3e771feb5c46de
Subrato Sarkar <subratosarkar@Subratos-MacBook-Pro.local>
2021-11-24 13:38:34 -0500

    js code added

--- js-concept/udemy/SECTION_16-Asynchronous.js/asynchrnous.js
+++ js-concept/udemy/SECTION_16-Asynchronous.js/asynchrnous.js
@@ -1,13 +1,24 @@
+//Synchronous code, because it executes line by line.
+const p = document.querySelector('.p');
+p.textContent = 'My name is Subrato';
+alert('Text set!');
+p.style.color = 'red' // this line won't be executed until user press Ok in alert message.
+
+//Asynchronous code
 console.log(1);
 setTimeout(() => {
     console.log(2);
-}, 1000);
-console.log(3);
-
+}, 1000); // Timer with callback
+console.log(3); //This line executed first before the previous line.
 /**o/p
  * 1
  * 3
  * 2
  */
 
-//Synchronous means: line by line execution
+/**
+ * Not all callback functions or callback functions alone don't make javascript automatically 
+asynchronous, only certain functions like setTimeout().
+ */
+
+//AJAX call is an example to communicate with remote web server asynchronously.
