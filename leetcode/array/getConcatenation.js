d29d526df33e14ee09e37fd1f3f440e4e151f606
subrato29 <subratosarkar29@gmail.com>
2021-12-31 15:04:50 -0500

    updated code

---
+++ leetcode/array/getConcatenation.js
@@ -0,0 +1,47 @@
+/**
+ * /**
+ * 1929. Concatenation of Array
+Easy
+
+590
+
+145
+
+Add to List
+
+Share
+Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
+
+Specifically, ans is the concatenation of two nums arrays.
+
+Return the array ans.
+
+Example 1:
+Input: nums = [1,2,1]
+Output: [1,2,1,1,2,1]
+Explanation: The array ans is formed as follows:
+- ans = [nums[0],nums[1],nums[2],nums[0],nums[1],nums[2]]
+- ans = [1,2,1,1,2,1]
+
+Example 2:
+Input: nums = [1,3,2,1]
+Output: [1,3,2,1,1,3,2,1]
+Explanation: The array ans is formed as follows:
+- ans = [nums[0],nums[1],nums[2],nums[3],nums[0],nums[1],nums[2],nums[3]]
+- ans = [1,3,2,1,1,3,2,1]
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+ var getConcatenation = function(nums) {
+    let result = [];
+    for (const num of nums) {
+        result.push(num);
+    }
+    for (const num of nums) {
+        result.push(num);
+    }
+    return result;
+};
