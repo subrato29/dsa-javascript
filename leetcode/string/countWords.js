541cd48157b04a4cf68515987e049dd1d5e81752
subrato29 <subratosarkar29@gmail.com>
2022-01-17 13:20:43 -0500

    added leetcode string problem

---
+++ leetcode/string/countWords.js
@@ -0,0 +1,60 @@
+/**
+ * 2085. Count Common Words With One Occurrence
+Easy
+
+175
+
+5
+
+Add to List
+
+Share
+Given two string arrays words1 and words2, return the number of strings that appear exactly once in each of the two arrays.
+
+Example 1:
+Input: words1 = ["leetcode","is","amazing","as","is"], words2 = ["amazing","leetcode","is"]
+Output: 2
+Explanation:
+- "leetcode" appears exactly once in each of the two arrays. We count this string.
+- "amazing" appears exactly once in each of the two arrays. We count this string.
+- "is" appears in each of the two arrays, but there are 2 occurrences of it in words1. We do not count this string.
+- "as" appears once in words1, but does not appear in words2. We do not count this string.
+Thus, there are 2 strings that appear exactly once in each of the two arrays.
+
+Example 2:
+Input: words1 = ["b","bb","bbb"], words2 = ["a","aa","aaa"]
+Output: 0
+Explanation: There are no strings that appear in each of the two arrays.
+
+Example 3:
+Input: words1 = ["a","ab"], words2 = ["a","a","a","ab"]
+Output: 1
+Explanation: The only string that appears exactly once in each of the two arrays is "ab".
+ */
+/**
+ * @param {string[]} words1
+ * @param {string[]} words2
+ * @return {number}
+ */
+ var countWords = function(words1, words2) {
+    let map = new Map();
+    for (const word1 of words1) {
+        if (map.has (word1)) {
+            map.set (word1, map.get(word1) + 1);
+        } else {
+            map.set (word1, 1);
+        }
+    }
+    for (const word2 of words2) {
+        if (map.has(word2) && map.get (word2) <= 1) {
+            map.set (word2, map.get (word2) - 1);
+        }
+    }
+    let counter = 0;
+    for (const [key, value] of map) {
+        if (map.get (key) == 0) {
+            counter++;
+        }
+    }
+    return counter;
+};
