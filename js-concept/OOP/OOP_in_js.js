f5c23a54957dc0edaf0ee7e2f38125efdd7a00fb
subrato29 <subratosarkar29@gmail.com>
2022-02-23 12:51:55 -0500

    OOP addec

---
+++ js-concept/OOP/OOP_in_js.js
@@ -0,0 +1,17 @@
+/**
+ * Constructor function: 
+ * - Technique to create object from function
+ * - This is how built-in objects like Arrays, Map, Set are actually implemented
+ */
+
+/**
+ * ES6 Classes
+ * - Modern alternative of Constructor function syntax
+ * - Works exactly like constructor functions
+ * - do NOT behave like classed like "classical OOP"
+ */
+
+/**
+ * Object.create ()
+ * - The easiest way of linking an object to a prototype object
+ */
