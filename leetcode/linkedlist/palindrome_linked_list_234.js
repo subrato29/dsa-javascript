f69e04142ffba8ce62a4d1bbfaf1ff9804f9257f
subrato29 <subratosarkar29@gmail.com>
2022-07-21 12:44:45 -0400

    leetcode problem added

---
+++ leetcode/linkedlist/palindrome_linked_list_234.js
@@ -0,0 +1,61 @@
+/**
+ * 234. Palindrome Linked List
+Easy
+
+9863
+
+600
+
+Add to List
+
+Share
+Given the head of a singly linked list, return true if it is a palindrome.
+
+Example 1:
+Input: head = [1,2,2,1]
+Output: true
+
+Example 2:
+Input: head = [1,2]
+Output: false
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+ var isPalindrome = function(head) {
+    let fast = head;
+    let slow = head;
+    while (fast != null && fast.next != null) {
+        fast = fast.next.next;
+        slow = slow.next;
+    }
+    slow = reverse (slow);
+    fast = head;
+    while (slow != null) {
+        if (fast.val != slow.val) {
+            return false;
+        }
+        slow = slow.next;
+        fast = fast.next;
+    }
+    return true;
+};
+
+function reverse (head) {
+    let prev = null;
+    while (head != null) {
+        let next = head.next;
+        head.next = prev;
+        prev = head;
+        head = next;
+    }
+    return prev;
+}
