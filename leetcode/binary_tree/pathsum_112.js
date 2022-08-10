2b13d5089b5d5406341b366a7878cee876f2dc92
subrato29 <subratosarkar29@gmail.com>
2022-08-10 11:48:53 -0400

    leetcode problem added

---
+++ leetcode/binary_tree/pathsum_112.js
@@ -0,0 +1,46 @@
+/**
+ * Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
+
+A leaf is a node with no children.
+
+Example 1:
+Input: root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
+Output: true
+Explanation: The root-to-leaf path with the target sum is shown.
+
+Example 2:
+Input: root = [1,2,3], targetSum = 5
+Output: false
+Explanation: There two root-to-leaf paths in the tree:
+(1 --> 2): The sum is 3.
+(1 --> 3): The sum is 4.
+There is no root-to-leaf path with sum = 5.
+
+Example 3:
+Input: root = [], targetSum = 0
+Output: false
+Explanation: Since the tree is empty, there are no root-to-leaf paths.
+ *
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+ var hasPathSum = function(root, targetSum) {
+    if (root == null) {
+        return false;
+    } else if (root.left == null && root.right == null && targetSum - root.val == 0) {
+        return true;
+    } else {
+        return hasPathSum (root.left, targetSum - root.val) || hasPathSum (root.right, targetSum - root.val);
+    }
+};
