78ac14998ef02f4369b8ae7da45d9272ca8f97db
Subrato Sarkar <ssarkar@SSARKAR2-MBP16.local>
2024-01-18 15:08:32 -0500

    leetcode 153

---
+++ typescript/leetcode/neetcode/leetcode_BLIND_75_Solutions/find_Minimum_In_Rotated_Sorted_Array_153.ts
@@ -0,0 +1,52 @@
+/**
+ * 153. Find Minimum in Rotated Sorted Array
+ *
+ * Medium
+ * Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, the array nums = [0,1,2,4,5,6,7] might become:
+ *
+ * [4,5,6,7,0,1,2] if it was rotated 4 times.
+ * [0,1,2,4,5,6,7] if it was rotated 7 times.
+ * Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
+ *
+ * Given the sorted rotated array nums of unique elements, return the minimum element of this array.
+ *
+ * You must write an algorithm that runs in O(log n) time.
+ *
+ *
+ *
+ * Example 1:
+ * Input: nums = [3,4,5,1,2]
+ * Output: 1
+ * Explanation: The original array was [1,2,3,4,5] rotated 3 times.
+ *
+ * Example 2:
+ * Input: nums = [4,5,6,7,0,1,2]
+ * Output: 0
+ * Explanation: The original array was [0,1,2,4,5,6,7] and it was rotated 4 times.
+ *
+ * Example 3:
+ * Input: nums = [11,13,15,17]
+ * Output: 11
+ * Explanation: The original array was [11,13,15,17] and it was rotated 4 times.
+ */
+
+function findMin(nums: number[]): number {
+  let left: number = 0;
+  let right: number = nums.length - 1;
+
+  while (left < right) {
+    const mid: number = Math.floor(left + (right - left) / 2);
+
+    // If the mid element is greater than the right most element,
+    // it means the minimum element is on the right side.
+    if (nums[mid] > nums[right]) {
+      left = mid + 1;
+    } else {
+      // If the mid element is less than or equal to the right most element,
+      // it means the minimum element is on the left side, or it's the mid element.
+      right = mid;
+    }
+  }
+
+  return nums[left];
+}
